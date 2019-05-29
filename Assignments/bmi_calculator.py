aee5657bdfe9fd9822cf91895319a4b2b960f637
Admin <mousavi.mehdi89@gmail.com>
2019-05-29 19:19:08 +0430

    prime edit 2.3

--- Assignments/bmi_calculator.py
+++ Assignments/bmi_calculator.py
@@ -6,30 +6,29 @@ if sys_unit == 'metric':
     wi_k = float(input('Please Enter Your Weight in Kilograms!'))
     bmi_m = wi_k / (he_m * he_m)
     if bmi_m <= 18.5:
-        print("your BMI is", bmi_m, "! you are underweight...\n"
-                                    "you need to put some meat on your bone :-|")
+        print("your BMI is", bmi_m, ". you are underweight...")
+
     elif bmi_m > 18.5 and bmi_m <= 25:
-        print("your BMI is", bmi_m, ". Congratulation!!! you are normal\n"
-                                    "keep doing what you do! stay healthy :-)")
+        print("your BMI is", bmi_m, ". you are normal")
+
     elif bmi_m > 25 and bmi_m <= 30:
-        print("Your BMI is", bmi_m, "! you are Overweight...\n"
-                                    "you need to hit the gym :-(")
+        print("Your BMI is", bmi_m, ". you have extra weight")
+
     elif bmi_m > 30:
-        print("Your BMI is", bmi_m, "wow, you are Obese !!! that means you have too much fat\n" \
-                                    "you need to stop eating unhealthy and start heavy exercising! good luck :-/")
+        print("Your BMI is", bmi_m, ". you are overweight")
+
 if sys_unit == 'imperial':
     he_i = float(input('Please Enter Your Height in Inches!'))
     wi_p = float(input('Please Enter Your Weight in Pounds!'))
     bmi_i = wi_p / (he_i * he_i) * 703
     if bmi_i <= 18.5:
-        print("your BMI is", bmi_i, "! you are underweight...\n"
-                                    "you need to put some meat on your bone :-|")
+        print("your BMI is", bmi_i, ". you are underweight")
+
     elif bmi_i > 18.5 and bmi_i <= 25:
-        print("your BMI is", bmi_i, ". Congratulation!!! you are normal\n"
-                                    "keep doing what you do! stay healthy :-)")
+        print("your BMI is", bmi_i, ". you are normal")
+
     elif bmi_i > 25 and bmi_i <= 30:
-        print("Your BMI is", bmi_i, "! you are Overweight...\n"
-                                    "you need to hit the gym :-(")
+        print("Your BMI is", bmi_i, ". you have extra weight")
+
     elif bmi_i > 30:
-        print("Your BMI is", bmi_i, "wow, you are Obese !!! that means you have too much fat\n" \
-                                    "you need to stop eating unhealthy and start heavy exercising! good luck :-/")
+        print("Your BMI is", bmi_i, ". you are overweight")
